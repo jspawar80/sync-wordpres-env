efc45fe7f0cbea3649a104914d24feae2cd74979
jspawar80 <127867978+jspawar80@users.noreply.github.com>
2024-04-07 02:35:39 +0530

    Update sync-working-directory.sh

--- scripts/sync-working-directory.sh
+++ scripts/sync-working-directory.sh
@@ -1,6 +1,13 @@
 # Define source and destination directories as variables
 source_dir="/home/jay/tpg-main-website/"
 destination_dir="/home/jay/tpg-main-website/"
+replace_url="http://localhost:10018"
+
+rm -rf bitnami_wordpress.sql
+
+wp search-replace  "$replace_url" 'https://dev.thinkproductgroup.com' --all-tables
+
+wp db export bitnami_wordpress.sql
 
 # Use rsync with variables
-rsync -ru --exclude="wp-config.php" --exclude="script.sh" --exclude="bitnami_wordpress.sql" "$source_dir"* "$destination_dir"
+rsync -ru --exclude="wp-config.php" --exclude="script.sh" "$source_dir"* "$destination_dir"
