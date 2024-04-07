45afbb2542a1314411253145c87b4e1d93b70f34
jspawar80 <127867978+jspawar80@users.noreply.github.com>
2024-04-07 22:14:07 +0530

    Update sync-working-directory.sh

--- scripts/sync-working-directory.sh
+++ scripts/sync-working-directory.sh
@@ -9,5 +9,7 @@ wp search-replace  "$replace_url" 'https://dev.thinkproductgroup.com' --all-tabl
 
 wp db export bitnami_wordpress.sql
 
+sed -i 's/localhost/127.0.0.1/g; s/local/bitnami_wordpress/g' bitnami_wordpress.sql
+
 # Use rsync with variables
 rsync -ru --exclude="wp-config.php" --exclude="script.sh" "$source_dir"* "$destination_dir"
